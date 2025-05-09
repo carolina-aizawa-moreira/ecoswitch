931dbe60fbd626a1605e179a873dba97f6d9675a
carolina-aizawa-moreira <carolina.aizawa.moreira@gmail.com>
2025-05-08 23:08:57 -0300

    commit inicial

---
+++ src/main/java/br/com/fiap/ecoswitch/ecoswitch/EcoswitchApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.ecoswitch.ecoswitch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoswitchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoswitchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/ecoswitch/ecoswitch/EcoswitchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.ecoswitch.ecoswitch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoswitchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
